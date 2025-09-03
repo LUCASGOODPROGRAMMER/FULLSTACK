b3d45ef13fab520f798a619856e3893747ce4e70
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-09-03 14:49:14 -0400

    variável

---
+++ aula006/index.js
@@ -0,0 +1,13 @@
+let nome; // inicializando variável como undefined, uma váriavel declarada sem valor
+nome = "lucas";
+console.log(`seja bem vindo ${nome}`);
+console.log("entre", nome);
+console.log("você nasceu em " + 2008);
+nome = "davi";
+console.log(nome)
+
+// sempre que um valor deve variar é bom usar let
+let nomeCliente = "gabriel";
+let codigoCliente = 102;
+let numeroCliente = "4324532-435";
+
