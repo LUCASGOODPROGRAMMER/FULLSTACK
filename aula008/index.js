fd3086b3ac43f2dbc0a69f7d54e954d43e39ce08
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-09-03 15:47:14 -0400

    var vs let

---
+++ aula008/index.js
@@ -0,0 +1,17 @@
+/*
+// var
+var nome = "gabriel"; // declarando um variável
+
+console.log(nome);
+
+var nome = "lucas"; // redeclarando a variável
+
+console.log(nome); // a redeclaração vai sobrescrever a primeira declaração
+
+console.log(nome);
+
+// let
+let nome = 'lucas';
+let nome = 'davi'; // o let não permite a redeclaração de uma variável
+nome = 'gabriel'; // apenas a modificação
+*/
