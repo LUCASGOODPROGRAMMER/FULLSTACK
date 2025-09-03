19ae49d98a932a4213c59c54b38eafa89d609a74
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-09-03 14:49:49 -0400

    variável constante

---
+++ aula007/index.js
@@ -0,0 +1,15 @@
+/** constantes
+ * variável que não pode ser modificada;
+ * variável que tem de ser inicializada e atribuida um valor imediatamente
+ */
+// const numero;
+const nome = "lucas";
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(resultadoTriplicado);
+console.log(typeof Number);
