dd808c94478dafd74976314fddd69dc19c85ce63
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-09-03 08:28:09 -0400

    console.log()

---
+++ aula003/script.js
@@ -0,0 +1,24 @@
+console.log("usando console.log('usando console.log()')"); // método log que esá dentro do objeto console
+
+// strings
+console.log("meu nome é lucas"); // aspas dupla
+console.log("meu nome é lucas"); // aspas simples
+console.log(`meu nome é lucas`); // aspas dinâmica
+
+const nome = "lucas";
+console.log(`meu nome é ${nome}`); // template strings
+console.log(`seja bem vindo ${nome}`); // template strings
+console.log(`com você está ${nome}`); // template strings
+
+//numbers
+console.log(2008); // posso passar númetos para os parâmetros
+console.log(29); // sem aspas ele converte automaticamente para número
+console.log(4); // por algumo motivo ele não aceita um zero com número inicial
+
+// números com ponto flutuante
+console.log(12345, 12.67); // o ponto flutuante é usado paraa separar as casas decimais em linguagens de programação e a virgula para passar mais de  um parâmetro
+
+console.log("lucas", 2008);
+console.log("eu nasci no ano de:", 2008);
+
+console.log('meu nome é "lucas". Estou aprendendo JavaScript ás', 8,"da manhã.");
