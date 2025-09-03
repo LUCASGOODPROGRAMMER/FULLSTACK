9becbaeb588ca2da6215bd4e1ce3abcb8856f691
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-09-03 08:49:43 -0400

    comentários

---
+++ aula004/comentarios.js
@@ -0,0 +1,12 @@
+// isso é um comentário ignorado pelo motor java script
+console.log('olá mundo!'); // outro comentário ff
+console.log('Linha 1');
+// não importa onde eu esteja, eu sou ignorado
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// ffffffffff
+/** outra forma de comentar
+ * bloco de código
+ * banana
+ */
