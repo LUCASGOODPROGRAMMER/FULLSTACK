a370daec56d54c38294e8cd4123b31b89d7f92e5
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-09-05 19:48:57 -0400

    alert e console

---
+++ aula011/js/script.js
@@ -0,0 +1,13 @@
+// alert("script debugado"); // método ou função do navegador pertencente ao objeto window (janela do navegador)
+
+/**
+ * window.alert() // um alerta
+ * window.confirm() // retorna um Boolean
+ * window.prompt() // formulario
+ */
+
+// dentro de um objeto é chamado de método e fora é chamado de função
+
+const numero1 = Number(prompt("digite um valor"));
+const numero2 = Number(prompt("digite um valor"));
+alert(`${numero1} + ${numero2} = ${numero1 + numero2}`);
